3ed1a911046e4cc4a108335db0e2613a217de192
iglée42 <igleev2@gmail.com>
2025-08-17 15:38:08 +0200

    Upload multiple images + Fix Some white mode text

--- src/components/adminpanel/CreateImgPopup.tsx
+++ src/components/adminpanel/CreateImgPopup.tsx
@@ -18,39 +18,67 @@ export function CreateImgPopup({ onClose, folders }: Props) {
     const sendImgUpdate = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         e.stopPropagation();
-        let toName = (folder ? folder + "/" : '') + name
-        const body = new FormData();
-        body.append("file", file!)
-        body.append("name",toName)
-        const res = await fetch(process.env.REACT_APP_API_URL + "/image", {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${localStorage.getItem("authToken")}`
-            },
-            body: body
-        })
-        if (res.ok) {
-            onClose();
-            showInfo("Image created successfully");
-            setTimeout(() => window.location.reload(), 1000);
+        if (!file) return;
+        if (file.name.endsWith(".zip")) {
+            const body = new FormData();
+            body.append("file", file)
+            const res = await fetch(process.env.REACT_APP_API_URL + "/images", {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("authToken")}`
+                },
+                body: body
+            })
+            if (res.ok) {
+                onClose();
+                showInfo("Images added successfully");
+                setTimeout(() => window.location.reload(), 1000);
+            } else {
+                showError("Failed to add images");
+            }
         } else {
-            showError("Failed to create image");
+            let toName = (folder ? folder + "/" : '') + name
+            const body = new FormData();
+            body.append("file", file!)
+            body.append("name", toName)
+            const res = await fetch(process.env.REACT_APP_API_URL + "/image", {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("authToken")}`
+                },
+                body: body
+            })
+            if (res.ok) {
+                onClose();
+                showInfo("Image created successfully");
+                setTimeout(() => window.location.reload(), 1000);
+            } else {
+                showError("Failed to create image");
+            }
         }
     }
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (!event.target.files) return;
-        setFile(event.target.files[0]);
-        setName(event.target.files[0].name)
+        const file: File = event.target.files[0];
+        setFile(file);
+        if (file.name.endsWith(".zip")) {
+            let name = document.getElementById("nameInput")! as HTMLInputElement
+            name.disabled = true
+            let folder = document.getElementById("folderInput")! as HTMLSelectElement
+            folder.disabled = true
+        } else {
+            setName(file.name)
+        }
     };
 
   return (
-    <Popup onClose={onClose} title={"Creating an image"}>
+    <Popup onClose={onClose} title={"Adding image(s)"}>
           <div className="pt-4 border-gray-200 dark:border-zinc-700 flex justify-center items-center flex-col">
               <input
                   type="file"
                   required={true}
-                  accept="image/png, image/gif"
+                  accept="image/png, image/gif, .zip"
                   onChange={(e) => handleFileChange(e)}
                   className="mt-2 w-full px-3 py-2 rounded-lg border border-gray-300 dark:border-zinc-700 bg-gray-50 dark:bg-gray-800 resize-none"
                   placeholder="Add file"
@@ -59,13 +87,15 @@ export function CreateImgPopup({ onClose, folders }: Props) {
                   required={true}
                   defaultValue={name}
                   onChange={(e) => setName(e.target.value)}
-                  className="mt-2 w-full px-3 py-2 rounded-lg border border-gray-300 dark:border-zinc-700 bg-gray-50 dark:bg-gray-800 resize-none"
+                  className="mt-2 w-full px-3 py-2 rounded-lg border border-gray-300 dark:border-zinc-700 bg-gray-50 dark:bg-gray-800 resize-none disabled:dark:bg-gray-900 disabled:bg-gray-200"
                   placeholder="Set the image name"
+                  id="nameInput"
               />
               <select
                   required={true}
-                  className="mt-2 w-full px-3 py-2 rounded-lg border border-gray-300 dark:border-zinc-700 bg-gray-50 dark:bg-gray-800 resize-none"
-                  onChange={(e)=> setFolder(e.target.value)}
+                  className="mt-2 w-full px-3 py-2 rounded-lg border border-gray-300 dark:border-zinc-700 bg-gray-50 dark:bg-gray-800 resize-none disabled:dark:bg-gray-900 disabled:bg-gray-200"
+                  onChange={(e) => setFolder(e.target.value)}
+                  id="folderInput"
               >
                   {Array.from(folders).map((f) => 
                       (<option value={f} selected={folder === f}>{f}</option>)
@@ -73,7 +103,7 @@ export function CreateImgPopup({ onClose, folders }: Props) {
               </select>
               <button
                   onClick={(e) => sendImgUpdate(e)}
-                  className=" mt-2 px-3 py-2 rounded-lg border border-gray-300 dark:border-zinc-700  bg-green-600 hover:bg-green-700 resize-none"
+                  className=" mt-2 px-3 py-2 rounded-lg border border-gray-300 dark:border-zinc-700  bg-green-600 hover:bg-green-700 resize-none text-white"
               >Save</button>
       </div>
     </Popup>
